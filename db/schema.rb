190ea8aec11fcfba82ade34c84903c3778bcbb4b
桑原亮介 <ryosuke0711993@gmail.com>
2018-06-06 12:36:22 +0900

    add country model and association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606033120) do
+ActiveRecord::Schema.define(version: 20180606033454) do
+
+  create_table "countries", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "home_reservations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "checkin_date", null: false
