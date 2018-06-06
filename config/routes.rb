f6b8fe0576bb1cb9c8e8e8a126cdc47c580601de
桑原亮介 <ryosuke0711993@gmail.com>
2018-06-06 15:24:26 +0900

    add comment outed foreign key

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+
+  get 'messages/new'
+
+  get 'messages/create'
+
+  get 'messages/edit'
+
+  get 'messages/update'
+
+  get 'messages/destroy'
+
   resources :home_reservations
   resources :home_reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
