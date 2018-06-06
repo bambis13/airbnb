c1b79670d39a54658999783f72b9cfc254f72814
関川智 <t.g.i.f.0312@gmail.com>
2018-06-06 19:37:17 +0900

    edit home association

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,10 @@
 class Home < ApplicationRecord
-   enum status: { visitor: 0, host: 1 }
+  enum status: { visitor: 0, host: 1 }
+
+  has_many                :favorites, :home_reservations, :home_reviews, :additional_home_rules
+  belongs_to              :home_category_sub, :room_type, :country, :currency, :user
+  has_one                 :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification
+  has_and_belongs_to_many :listing_photos
+
+  accepts_nested_attributes_for :favorites, :home_reservations, :home_reviews, :additional_home_rules, :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification
 end
