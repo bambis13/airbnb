0062ff1a5502c67caab7cb5174948e65ecd21a51
桑原亮介 <ryosuke0711993@gmail.com>
2018-06-06 17:47:23 +0900

    remove use model

--- app/views/users/_user.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! user, :id, :name, :birth_day, :sex, :phone_number, :postal_code, :prefeture, :town, :street, :building, :self_introduction, :created_at, :updated_at
-json.url user_url(user, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   resources :home_reservations
   resources :home_reviews
 
-  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
