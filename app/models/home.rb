210667554f47bbc58d297ec3b7ddb1ea2e8e0bdb
関川智 <t.g.i.f.0312@gmail.com>
2018-06-06 20:08:20 +0900

    add accepts_nested_attributes_for option

--- app/models/home.rb
+++ app/models/home.rb
@@ -6,5 +6,9 @@ class Home < ApplicationRecord
   has_one                 :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification
   has_and_belongs_to_many :listing_photos
 
-  accepts_nested_attributes_for :favorites, :home_reservations, :home_reviews, :additional_home_rules, :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification
+  accepts_nested_attributes_for :favorites, :home_reservations, :home_reviews, :additional_home_rules, :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification, allow_destroy: true, reject_if: :reject_additional_home_rules
+
+  def reject_additional_home_rules(attributes)
+    attributes['content'].blank?
+  end
 end
