51b518f0320889f06882bee6f3b8d3f628aacf29
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-07 19:27:12 +0900

    delete invite_frequency column from homes

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,6 +1,4 @@
 class Home < ApplicationRecord
-  enum status: { visitor: 0, host: 1 }
-
   has_many                :favorites, :home_reservations, :home_reviews, :additional_home_rules
   belongs_to              :home_category_sub, :room_type, :country, :currency, :user
   has_one                 :amenity, :bed_type, :available_spaces, :overview, :available_setting, :price, :home_rule, :additional_home_rule, :home_notification
