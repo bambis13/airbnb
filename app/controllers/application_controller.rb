b12fc2a4b5a7450d7d759b80579560e10e020528
katsunori-takahashi <[k.takahashi.0105@gmail.com]>
2018-06-08 17:11:31 +0900

    add devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :birth_day, :phone_number, :postal_code, :delf_introduction, :prefecture, :town, :street, :building])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :birth_day, :phone_number, :postal_code, :self_introduction, :prefecture, :town, :street, :building, :country, :language, :currency])
   end
 end
