19c410ee517a2796602477f7e5c46a87639229e9
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-09 13:50:51 +0900

    add scss application.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  get 'messages/index'
-
-  get 'messages/new'
-
-  get 'messages/create'
-
-  get 'messages/edit'
-
-  get 'messages/update'
-
-  get 'messages/destroy'
+  root 'homes#index'
+  resources :messages
   resources :home_reviews
+  # resources :devise_users
   # resources :home do
   resources :home_reservations, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
