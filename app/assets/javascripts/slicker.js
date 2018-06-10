973f71e6d0b9c47487e73303e1fbf6ff776d1f40
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-10 19:21:54 +0900

    complete trip-with-airbnb

--- app/assets/javascripts/slicker.js
+++ app/assets/javascripts/slicker.js
@@ -2,6 +2,7 @@ $(function() {
     $('.listing-slick').slick({
       accessibility: true,
       infinite: true,
+      fade: true,
       dots:true,
       slidesToShow: 1,
       slidesToScroll: 1,
