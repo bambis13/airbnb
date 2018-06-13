4f321356e9c0ba21051e1347016279a86324f41a
katsunori-takahashi <[k.takahashi.0105@gmail.com]>
2018-06-13 12:22:26 +0900

    created welcome_session

--- Gemfile
+++ Gemfile
@@ -59,10 +59,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-<<<<<<< HEAD
 gem "haml"
-=======
->>>>>>> signup_func
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'jquery-ui-rails'
 gem 'mini_magick'
@@ -71,8 +68,4 @@ gem 'haml-rails'
 gem 'erb2haml'
 gem 'kaminari'
 gem 'mechanize'
-<<<<<<< HEAD
 gem 'devise'
-=======
-gem 'devise'
->>>>>>> signup_func
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-<<<<<<< HEAD
 
-=======
   has_many :favorite_lists
   has_many :listing_photos
   has_many :messages
@@ -14,5 +12,5 @@ class User < ApplicationRecord
   belongs_to :language
   belongs_to :country
   belongs_to :currency
->>>>>>> signup_func
+
 end
