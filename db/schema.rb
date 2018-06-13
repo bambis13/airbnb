b9a4c1c92948a483408fe29872c2f3c2de033de0
関川智 <t.g.i.f.0312@gmail.com>
2018-06-13 18:47:06 +0900

    最新状態にマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -133,8 +133,6 @@ ActiveRecord::Schema.define(version: 20180612074859) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "home_id", null: false
-    t.index ["home_id"], name: "index_home_category_subs_on_home_id"
     t.index ["name"], name: "index_home_category_subs_on_name"
   end
 
