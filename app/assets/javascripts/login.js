185aab9fb15982e005050b4f322ce064d30a7491
katsunori-takahashi <[k.takahashi.0105@gmail.com]>
2018-06-14 16:28:38 +0900

    checkout branch

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -3,4 +3,14 @@ $(function(){
   loginAction.click(function(){
     $('#login_modal').fadeIn();
   })
+  var mailBer = $('#mail_login')
+  mailBer.on('click', function(){
+    $('.login_contents').hide();
+  })
+  var redirectSignup = &('#signup_action')
+  var signupSession = &('')
+  redirectSignup.on('click', function(){
+    $('.login_contents').hide();
+
+  })
 })
