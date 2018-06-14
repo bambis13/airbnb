744750e2b087380ed235bce689d45ea6b198d470
katsunori-takahashi <[k.takahashi.0105@gmail.com]>
2018-06-14 18:19:50 +0900

    created_modal en_route

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,16 +1,63 @@
 $(function(){
-  var loginAction = $('#login_action')
-  loginAction.click(function(){
-    $('#login_modal').fadeIn();
-  })
-  var mailBer = $('#mail_login')
-  mailBer.on('click', function(){
-    $('.login_contents').hide();
-  })
-  var redirectSignup = &('#signup_action')
-  var signupSession = &('')
-  redirectSignup.on('click', function(){
-    $('.login_contents').hide();
 
+  var firstLoginSection = $('.login_contents')
+  var loginAction = $('#login_action')
+  var loginModel = $('#login_model')
+  var mailBer = $('#mail_login')
+  var redirectLogin = $('.login_session_contents')
+  var redirectSignup = $('#signup_action')
+  var signupSession = $('') //間違えて消してしまったsignupファイル"後で作る"
+  var registrationMailSection = $('') //消してしまったsignupファイルのmailbtn
+  var loginMailSession = $('.signup_registration_contents')
+  var redirectLoginBtn = $('redirect_login_btn')
+  var actionPassword = $('.click_password')
+
+  loginAction.click(function(){
+    loginModel.fadeIn();
+  })
+  mailBer.on('click', function(){
+    firstLoginSection.hide();
+    redirectLogin.fadeIn();
+  })
+  redirectSignup.on('click', function(){
+    firstLoginSection.hide();
+    signupSession.fadeIn();
+    registrationMailSection.on('click',function(){
+      signupSession.hide();
+      loginMailSession.fadeIn();
+      actionPassword.on('click', function(){
+        actionPassword.append(PasswordFiledHTML)
+          PasswordFiledHTML =
+               `<div class='field_password_certifcation'>
+                  <div class='field_password_certifcation__mark'>
+                  </div>
+                  <div class='field_password_certifcation__text'>
+                  </div>
+                </div>
+                <div class='field_password_certifcation'>
+                  <div class='field_password_certifcation__mark'>
+                  </div>
+                  <div class='field_password_certifcation__text'>
+                  </div>
+                </div>
+                <div class='field_password_certifcation'>
+                  <div class='field_password_certifcation__mark'>
+                  </div>
+                  <div class='field_password_certifcation__text'>
+                  </div>
+                </div>
+                <div class='field_password_certifcation'>
+                  <div class='field_password_certifcation__mark'>
+                  </div>
+                  <div class='field_password_certifcation__text'>
+                  </div>
+                </div>`
+        //field_password_certifcationのmarkとtextは非同期で表示が変わる（正規表現で取得)
+        redirectLoginBtn.on('click',function(){
+          loginMailSession.hide();
+          firstLoginSection.fadeIn();
+        })
+      })
+    })
   })
 })
