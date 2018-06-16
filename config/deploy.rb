02d829252095d5c3cccbebca0ad6a6bdd52545a9
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-16 16:57:40 +0900

    set :pty, true

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock '3.11.0'
 
 set :application, 'airbnb'
 set :repo_url,  'git@github.com:bambis13/airbnb.git'
+set :pty, true
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :linked_files, %w{ config/secrets.yml }
