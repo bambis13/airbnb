aaf7641acccdf90b31fc3bc8bbe6cf0e9a6e8c6d
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-16 12:22:09 +0900

    slick not found error解消

--- app/assets/javascripts/slicker.js
+++ app/assets/javascripts/slicker.js
@@ -1,21 +1,22 @@
 $(function(){
 
-  $('.listing-slick').hover(
-    function(){
-      $(this).children('.slick-arrow').css("visibility", "visible");
-    },
-    function(){
-      $(this).children('.slick-arrow').css("visibility", "hidden");
-    }
-  );
-  $('.listing-slick').slick({
-      accessibility: true,
-      infinite: true,
-      fade: true,
-      dots:true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-     });
+//listing-s
+$('.listing-slick').hover(
+  function(){
+    $(this).children('.slick-arrow').css("visibility", "visible");
+  },
+  function(){
+    $(this).children('.slick-arrow').css("visibility", "hidden");
+  }
+);
+$('.listing-slick').slick({
+    accessibility: true,
+    infinite: true,
+    fade: true,
+    dots:true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+   });
 });
 
 //複数枚表示
