6338bbb95fc6b1a0e7226c57d2ad1e8c55286edf
白柳明香里 <asm13.5taykox@gmail.com>
2018-06-22 02:06:26 +0900

    スクロール実装途中

--- app/assets/javascripts/home_reservations.js
+++ app/assets/javascripts/home_reservations.js
@@ -64,15 +64,11 @@ $(function(){
         }else{
           $('.form-texts').append("<p class=\"form-text-children\">, 子ども1人</p>");
         }
-        // $('.form-text-children').remove();
-        // $('.form-texts').append("<p class=\"form-text-children\">, 子ども" + countChildren + "人</p>");
       }else{
         countChildren += 1;
         $('input:hidden[name="number-of-children-sa"]').val(countChildren);
         $('.number-of-children-html').text(countChildren);
         $('.form-text-children').text(", 子ども" + countChildren + "人");
-        // $('.form-text-children').remove();
-        // $('.form-texts').append("<p class=\"form-text-children\">, 子ども" + countChildren + "人</p>");
         $(this).prop("disabled", true);
         $(".count-up-adult").prop("disabled", true);
       }
@@ -83,18 +79,16 @@ $(function(){
       $(".count-up-children").prop("disabled", false);
       var countChildren = parseInt($('input:hidden[name="number-of-children-sa"]').val());
       if (countChildren > 1){
-      countChildren -= 1;
-      $('input:hidden[name="number-of-children-sa"]').val(countChildren);
-      $('.number-of-children-html').text(countChildren);
-      $('.form-text-children').text(", 子ども" + countChildren + "人");
-      // $('.form-text-children').remove();
-      // $('.form-texts').append("<p class=\"form-text-children\">, 子ども" + countChildren + "人</p>");
+        countChildren -= 1;
+        $('input:hidden[name="number-of-children-sa"]').val(countChildren);
+        $('.number-of-children-html').text(countChildren);
+        $('.form-text-children').text(", 子ども" + countChildren + "人");
       }else{
-      countChildren -= 1;
-      $('input:hidden[name="number-of-children-sa"]').val(countChildren);
-      $('.number-of-children-html').text(countChildren);
-      $('.form-text-children').remove();
-      $(this).prop("disabled", true);
+        countChildren -= 1;
+        $('input:hidden[name="number-of-children-sa"]').val(countChildren);
+        $('.number-of-children-html').text(countChildren);
+        $('.form-text-children').remove();
+        $(this).prop("disabled", true);
       }
     });//子供down閉じ
 
@@ -151,18 +145,32 @@ $(function(){
     $('#rotate').addClass('fa-angle-down');
     $('#select-guests-num').css('visibility','hidden');
   });
+  //スクロール固定
+  // $(window).on('load', function () {
+  //     // $navSetTop = $navSet.offset().top;
+  //   $(window).on('scroll', function () {
+  //     var $navSet = $('.nav-set').offset().top;
+  //     console.log($navSet);
+  //     if($navSet < 90) {
+  //       $('.nav-set').addClass('is-fixed');
+  //     } else {
+  //       $('.nav-set').removeClass('is-fixed');
+  //     }
+  //   });
+  // });
 
-  //料金計算
-  $("input[type='text']").change(function(){
-    var checkin = Date.parse($('.reservation-checkin').val());
-    var checkout = Date.parse($('.reservation-checkout').val());
-    days = (checkout - checkin)/1000/60/60/24;
-    var defaultFee = $('input:hidden[name="default_fee"]').val();
-    var additionalFee = $('input:hidden[name="additional_guests_fee"]').val();
-    var cleaningFee = $('input:hidden[name="cleaning_fee"]').val();
-    var serviceFee = $('input:hidden[name="service_fee"]').val();
-    var totalFee = (defaultFee * days)+ cleaningFee + serviceFee
-    var additionalFeeFrom = $('.additional_fee_from').val();
-  });
+  // //料金計算
+  // $("input[type='text']").change(function(){
+  //   console.log("changed!");
+  //   var checkin = Date.parse($('.reservation-checkin').val());
+  //   var checkout = Date.parse($('.reservation-checkout').val());
+  //   days = (checkout - checkin)/1000/60/60/24;
+  //   var defaultFee = $('input:hidden[name="default_fee"]').val();
+  //   var additionalFee = $('input:hidden[name="additional_guests_fee"]').val();
+  //   var cleaningFee = $('input:hidden[name="cleaning_fee"]').val();
+  //   var serviceFee = $('input:hidden[name="service_fee"]').val();
+  //   var totalFee = (defaultFee * days)+ cleaningFee + serviceFee
+  //   var additionalFeeFrom = $('.additional_fee_from').val();
+  // });
 
 });//一番上のfunction閉じ
