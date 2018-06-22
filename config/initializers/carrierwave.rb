bec946b564ae75359f223bf6579fe534f98b7261
関川智 <t.g.i.f.0312@gmail.com>
2018-06-22 14:18:11 +0900

    edit config_carrierwave
    ローカル環境ではdb、本番環境ではS3から画像を取得するよう設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,20 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+if Rails.env.production?
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
+  require 'carrierwave/storage/abstract'
+  require 'carrierwave/storage/file'
+  require 'carrierwave/storage/fog'
 
-  config.fog_directory  = 'techexp-airbnb'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techexp-airbnb'
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+
+    config.fog_directory  = 'techexp-airbnb'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techexp-airbnb'
+  end
 end
