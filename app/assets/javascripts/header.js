6b8a85df6845c46b9c20d6da9e3863e4bcb9a46a
katsunori-takahashi <[k.takahashi.0105@gmail.com]>
2018-06-30 14:32:19 +0900

    add header js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -46,3 +46,15 @@
 //     return false
 //   })
 // });
+
+$(function(){
+  const hiddenMenu = $('#hidden_menu')
+  const userField = $('#user_field')
+  hiddenMenu.on('mouseover', function(){
+    userField.fadeIn();
+    event.preventDefault();
+  })
+  $('.contents').on('click', function(){
+    userField.fadeOut();
+  })
+})
