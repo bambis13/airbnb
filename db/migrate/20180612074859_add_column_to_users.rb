09760f432b0cedfaa97db7fe26d158cccc02f602
白柳明香里 <asm13.5taykox@gmail.com>
2018-07-04 21:49:03 +0900

    migration fileのインデント揃え忘れ修正

--- db/migrate/20180612074859_add_column_to_users.rb
+++ db/migrate/20180612074859_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :status, :integer, null: false
+    add_column :users, :status,    :integer, null: false
     add_column :users, :superhost, :integer, default: 0
   end
 end
